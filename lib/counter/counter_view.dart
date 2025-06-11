806cf9fb3c8e8abda8783bb09c554a37d3a4e0c3
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-11 12:55:25 +0200

    add functionality

--- lib/counter/counter_view.dart
+++ lib/counter/counter_view.dart
@@ -1,10 +1,37 @@
+import 'package:bloc_basics/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterView extends StatelessWidget {
   const CounterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: BlocBuilder<CounterCubit, int>(
+        builder: (context, state) {
+          return Center(
+            child: Text(
+              state.toString(),
+              style: TextStyle(fontSize: 50),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().increment(),
+            child: Icon(Icons.add),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().decrement(),
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
   }
 }
