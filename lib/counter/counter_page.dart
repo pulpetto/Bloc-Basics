05b8cd595f0461d7f677a4fda8efd4e9ce7ad649
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-11 12:48:50 +0200

    add first bloc

---
+++ lib/counter/counter_page.dart
@@ -0,0 +1,16 @@
+import 'package:bloc_basics/counter/counter_cubit.dart';
+import 'package:bloc_basics/counter/counter_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(0),
+      child: CounterView(),
+    );
+  }
+}
---
+++ lib/counter/counter_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
